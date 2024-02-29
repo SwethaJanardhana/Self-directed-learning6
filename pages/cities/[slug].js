b937deabe57bb93ec6a3594375b6112c54edf5fe
Swetha Janardhana <SwethaJanardhana85@gmail.com>
2024-02-29 12:51:11 +0100

    implemented icons

--- pages/cities/[slug].js
+++ pages/cities/[slug].js
@@ -2,9 +2,20 @@ import { useRouter } from "next/router";
 import { cities } from "@/lib/data";
 import StyledLink from "@/components/StyledLink";
 import StyledSection from "@/components/StyledSection";
+import Image from "next/image";
+import { styled } from "styled-components";
+import Head from "next/head";
+
+const StyledImage = styled(Image)`
+  fill: white;
+`;
 
 export default function CityDetails() {
   const router = useRouter();
+
+  if (!router.query) {
+    return;
+  }
   const { slug } = router.query;
 
   const {
@@ -15,15 +26,27 @@ export default function CityDetails() {
   } = cities.find((city) => city.slug === slug);
 
   return (
-    <StyledSection>
-      <h1>{country}</h1>
-      <h2>Capital City : {capital}</h2>
-      <p>Population : {population}</p>
-      <article>
-        <p>{description}</p>
-      </article>
-      <br />
-      <StyledLink href="/cities">All Cities</StyledLink>
-    </StyledSection>
+    <>
+      <Head>
+        <title>{country}</title>
+      </Head>
+      <StyledSection>
+        <h1>{country}</h1>
+        <h2>Capital City : {capital}</h2>
+        <p>Population : {population}</p>
+        <article>
+          <p>{description}</p>
+        </article>
+        <br />
+        <StyledLink href="/cities">
+          <StyledImage
+            src="/assets/left-arrow.svg"
+            alt="back arrow"
+            height={20}
+            width={20}
+          />
+        </StyledLink>
+      </StyledSection>
+    </>
   );
 }
--- pages/cities/index.js
+++ pages/cities/index.js
@@ -3,20 +3,49 @@ import Link from "next/link";
 import StyledList from "@/components/StyledList";
 import StyledListItems from "@/components/StyledListItems";
 import { styled } from "styled-components";
+import Head from "next/head";
+import Heart from "@/Icons/heart.svg";
+import RightArrow from "@/Icons/right-arrow.svg";
+import Image from "next/image";
+
+const StyledImage = styled(Image)`
+  fill: white;
+`;
+
+const StyledHeart = styled(Heart)`
+  width: 15px;
+  fill: "pink";
+`;
 
 const StyledLink = styled(Link)`
   color: white;
+  text-decoration: none;
 `;
 
 export default function HomePage() {
   return (
     <main>
+      <Head>
+        <title>Cities</title>
+      </Head>
       <StyledList>
         {cities.map((city) => (
           <StyledListItems key={city.id}>
-            <StyledLink href={`cities/${city.slug}`}>{city.country}</StyledLink>
+            <StyledLink href={`cities/${city.slug}`}>
+              <RightArrow />
+              {city.country}
+              <StyledHeart />
+            </StyledLink>
           </StyledListItems>
         ))}
+        <StyledLink href="/">
+          <StyledImage
+            src="/assets/left-arrow.svg"
+            alt="back arrow"
+            height={20}
+            width={20}
+          />
+        </StyledLink>
       </StyledList>
     </main>
   );
